13b43be1a7eefd6689ebf39ff4eee64bdae381f1
Pixels-1 <166007314+Pedrollaverrissie@users.noreply.github.com>
2026-06-16 08:45:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function show() {
+    const menuPopup =document.querySelector('.right');
+    const showMenu = document.querySelector('.menu-tab');
+    showMenu.classList.toggle('active');
+
+    }
+
+function loginpage(){
+    document.querySelector(".login-popup").style.display="block";
+      
+};
